071edf74208be7201d541113269310a169bbc5ce
manolo <emmanuelripoll@gmail.com>
2017-03-26 12:25:20 +0200

    add weekend planning

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new TeamBundle\TeamBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new PlanningBundle\PlanningBundle(),
+            new PlaceBundle\PlaceBundle(),
             new InfoBundle\InfoBundle(),
             new PlanningBundle\PlanningBundle(),
             new PlaceBundle\PlaceBundle(),
