1e9c49e94e5856b7767f96393cd136473e922e58
Alex7up <thorel.alexandre@gmail.com>
2017-03-28 16:52:02 +0200

    bloqué affichage history public

---
+++ src/AppBundle/Controller/HistoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use InfoBundle\Entity\History;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+
+
+
+class HistoryController extends Controller
+{
+    public function indexAction()
+    {
+      $em = $this->getDoctrine()->getManager();
+
+      $histories = $em->getRepository('InfoBundle:History')->findAll();
+
+        return $this->render('AppBundle:page:history.html.twig', ['histories' => $histories]);
+    }
+}
