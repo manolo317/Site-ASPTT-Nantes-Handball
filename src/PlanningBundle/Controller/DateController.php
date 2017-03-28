c1ca4af60d0595a0c8d9e70d33def3eeb7e3ed5d
manolo <emmanuelripoll@gmail.com>
2017-03-28 15:04:34 +0200

    update DateController

--- src/PlanningBundle/Controller/DateController.php
+++ src/PlanningBundle/Controller/DateController.php
@@ -5,7 +5,8 @@ namespace PlanningBundle\Controller;
 use PlanningBundle\Entity\DateEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * DateEvent controller.
